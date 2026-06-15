79e00ca6c0d4c14ed83489f4fd5bdcf9cb6e2354
Palo Larrain <palolarrain@gmail.com>
2026-06-15 09:45:10 -0400

    Initial commit

---
+++ src/app/Main.java
@@ -0,0 +1,37 @@
+package ui;
+
+import data.GestorDatos;
+import model.Tour;
+
+import java.util.ArrayList;
+
+/**
+ * Clase principal del sistema Llanquihue Tour.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        GestorDatos gestorDatos = new GestorDatos();
+
+        String rutaArchivo = "resources/tours.txt";
+
+        ArrayList<Tour> tours =
+                gestorDatos.cargarTours(rutaArchivo);
+
+        System.out.println("=== TODOS LOS TOURS ===");
+
+        for (Tour tour : tours) {
+            System.out.println(tour);
+        }
+
+        System.out.println();
+        System.out.println("=== TOURS GASTRONÓMICOS ===");
+
+        for (Tour tour : tours) {
+            if (tour.getTipo().equalsIgnoreCase("Gastronómico")) {
+                System.out.println(tour);
+            }
+        }
+    }
+}
